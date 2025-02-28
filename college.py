6615c9683c37c4a43fddbc03eb6e87f99406f6fd
V-Meenakshi <152772510+V-Meenakshi@users.noreply.github.com>
2025-02-28 14:51:43 +0530

    Update college.py

--- college.py
+++ college.py
@@ -15,7 +15,7 @@ except Exception as e:
 df = df.fillna("")
 df['Question'] = df['Question'].str.lower()
 df['Answer'] = df['Answer'].str.lower()
-vectorizer = Tfidfvectorizer()
+vectorizer = TfidfVectorizer()
 question_vectors = vectorizer.fit_transform(df['Question'])
 API_KEY="AIzaSyByLfEfaGdpdZHHdcPgbQeRAISAFqFrv4s"
 genai.configure(api_key=API_KEY)
