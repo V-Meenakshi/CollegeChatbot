46dc4ddac785b8433f54e9c0bdf277bfdf931a32
V-Meenakshi <152772510+V-Meenakshi@users.noreply.github.com>
2025-02-28 15:48:28 +0530

    Update college.py

--- college.py
+++ college.py
@@ -34,7 +34,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Configure models
-genai.configure(api_key="AIzaSyBsq5Kd5nJgx2fejR77NT8v5Lk3PK4gbH8")
+genai.configure(api_key="AIzaSyByLfEfaGdpdZHHdcPgbQeRAISAFqFrv4s")
 gemini = genai.GenerativeModel('gemini-1.5-flash')
 embedder = SentenceTransformer('all-MiniLM-L6-v2')  # Embedding model
 
